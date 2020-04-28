6eceead79414fb66ec98a6e194abfd0d936e395b
naoto64 <51907267+naoto64@users.noreply.github.com>
2020-04-28 16:03:48 +0900

    Update RainSense.cpp

--- src/RainSense.cpp
+++ src/RainSense.cpp
@@ -1,6 +1,7 @@
 /*
  * RainSense.cpp
  */
+#include <Arduino.h>
 #include "RainSense.h"
 
 RainSense::RainSense(uint8_t pin) {
@@ -17,17 +18,7 @@ byte RainSense::value(byte samples, byte sensitivity) {
   if(samples < 1){
     samples = 1;
   }
-  for(int i = 0; i < sensitivity; i++) {
-    int Y = 1023 * samples;
-    for (size_t j = 0; j < samples; j++) {
-      Y -= analogRead(_pin);
-    }
-    Y /= samples;
-    sumXY += i * Y;
-    sumX += i;
-    sumY += Y;
-    sumX2 += i * i;
-  }
+  sum(samples, sensitivity, &sumXY, &sumX, &sumY, &sumX2);
   val = (sensitivity * sumXY - sumX * sumY) / (sensitivity * sumX2 - sumX * sumX);
   if(val < 0) val = 0;
   if(val > 100) val = 100;
@@ -39,5 +30,24 @@ boolean RainSense::rain(byte threshold, byte samples, byte sensitivity) {
     return 1;
   } else {
     return 0;
+  }
+}
+
+int RainSense::getY(byte samples) {
+  int Y = 1023 * samples;
+  for (size_t j = 0; j < samples; j++) {
+    Y -= analogRead(_pin);
+  }
+  Y /= samples;
+  return Y;
+}
+
+void RainSense::sum(byte samples, byte sensitivity, int* sumXY, int* sumX, int* sumY, int* sumX2) {
+  for(int i = 0; i < sensitivity; i++) {
+    int Y = getY(samples);
+    *sumXY += i * Y;
+    *sumX += i;
+    *sumY += Y;
+    *sumX2 += i * i;
   }
 }
