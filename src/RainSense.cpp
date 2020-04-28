d88a030cd0d2975088b20924d9c47c1a2dc1a07d
naoto64 <51907267+naoto64@users.noreply.github.com>
2020-04-28 14:37:17 +0900

    Update RainSense.cpp

--- src/RainSense.cpp
+++ src/RainSense.cpp
@@ -17,7 +17,7 @@ byte RainSense::value(byte samples, byte sensitivity) {
   if(samples < 1){
     samples = 1;
   }
-  sum(samples, sensitivity, &sumXY, &sumX, &sumY, &sumX2);
+  sum(samples, sensitivity, sumXY, sumX, sumY, sumX2);
   val = (sensitivity * sumXY - sumX * sumY) / (sensitivity * sumX2 - sumX * sumX);
   if(val < 0) val = 0;
   if(val > 100) val = 100;
