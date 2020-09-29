d71f98fbf68f13a2da786bd300f9b12b99ca0fb1
naoto64 <51907267+naoto64@users.noreply.github.com>
2020-09-29 20:34:02 +0900

    Update RainSense.h

--- src/RainSense.h
+++ src/RainSense.h
@@ -8,7 +8,7 @@ class RainSense {
 public:
   RainSense(uint8_t pin);
   byte value(byte samples=1, byte sensitivity=16);
-  boolean rain(byte threshold, byte samples=1, byte sensitivity=16);
+  boolean rain(byte threshold, byte sensitivity=192);
 
 private:
   int getY(byte samples);
